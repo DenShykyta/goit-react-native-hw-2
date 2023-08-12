e0151bd81495148678f223a1fc41fde2b1381f32
DenShykyta <wrkacc4@gmail.com>
2023-08-13 00:00:23 +0300

    fix and update
    - fix and update some styles parameters
    - update the file and folder structure
    - add keyboard close outside keyboard

--- Screens/index.js
+++ Screens/index.js
@@ -1,3 +1,3 @@
-export { PostsScreen } from "./PostsScreen";
-export { RegistrationScreen } from "./RegistrationScreen";
+export { PostsScreen } from "./PostsScreen/PostsScreen";
+export { RegistrationScreen } from "./RegScreen";
 export { LoginScreen } from "./LoginScreen";
